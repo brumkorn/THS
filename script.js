3b5157e654aacbd609359f05f8afe6ccdf57fea1
brumkorn <brumkorn@gmail.com>
2016-03-30 19:13:02 +0300

    Draw full table with js by set row and columns quontity

--- script.js
+++ script.js
@@ -2,19 +2,49 @@
     "use dtrict";
 
     let table = document.getElementById("table");
-    let header = table.createTHead();
-    let row = table.insertRow(0);
-
-    addHeader();
     
-    function addHeader() {
-        for (let i = 0; i < 11; i++) {
+
+    addHeader(15);
+    addRows(10);
+
+    /*Create header for the table*/
+    function addHeader(quontity) {
+        let header = table.createTHead();
+        header.id = "header";
+        let row = header.insertRow(-1);
+        for (let i = 0; i <= quontity; i++) {
+            let cell;
+
             if (i === 0) {
                 row.insertCell(i);
                 continue;
             }
-            let cell = row.insertCell(i);
+
+            cell = row.insertCell(i);
             cell.innerHTML = String.fromCharCode("A".charCodeAt(0) + i - 1);
+        }
+    }
+
+    function addRows(quontity) {
+        let body = table.appendChild( document.createElement("tbody") );
+        let columns = document.getElementById("header").firstChild;
+        columns = columns.childElementCount - 1;
+
+        for (let i = 0; i < quontity; i++) {
+            let row = body.insertRow(-1);
+
+            for (let j = 0; j <= columns; j++) {
+                let cell;
+
+                if (j === 0) {
+                    let cell = row.insertCell(j);
+                    cell.innerHTML = i+1;
+                    continue;
+                }
+
+                cell = row.insertCell(j);
+                cell.innerHTML = "<input type='text'>";
+            }
         }
     }
 
