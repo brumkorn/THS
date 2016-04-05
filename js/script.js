6e5ae93f2299712eea43340dd967fc416ae3d19f
brumkorn <brumkorn@gmail.com>
2016-04-05 19:50:53 +0300

    do saving to local storage basic formulas

--- js/script.js
+++ js/script.js
@@ -14,19 +14,23 @@
 ;(function() {
     "use dtrict";
     createTable();
+    loadData();
+    enterData();
 
-/*
-*Create table with speifying number of columns and rows.
-*Table first row is tHead with column naming.
-*Table first column is header with row index.
-*Other table body consists of cells.
-*
-*Each input tag have ID wich consists of column letter and row index.
-*
-*@param columns - integer number of columns
-*@param rows - integer number of rows
-*/
+    /*
+    *Create table with speifying number of columns and rows.
+    *Table first row is tHead with column naming.
+    *Table first column is header with row index.
+    *Other table body consists of cells.
+    *
+    *Each input tag have ID wich consists of column letter and row index.
+    *
+    *@param columns - integer number of columns
+    *@param rows - integer number of rows
+    */
 
+    // Seems Need to refactor. Dont like this complex nested loop with so much if
+    // There must be an error.
     function createTable(columns, rows) {
         rows = rows || 1000;
         columns = columns || 26;
@@ -69,47 +73,103 @@
                 }
             }
         }
+    }
 
+    /*
+    *
+    *
+    *Invoking input by double click on cell. Entering data, saving it to 
+    *local storage and computing formulas after '=' sign.
+    *
+    *
+    */
 
-        table.addEventListener("dblclick", dblClickCell);
+    //Maybe need to separate into few functions
+    function enterData() {
+        var table = document.getElementById("table");
 
-/*
-* Event handler for dblclick Listner
-*/
-        function dblClickCell(e) {
+        table.addEventListener("dblclick", invokeInput);
+        table.addEventListener("keydown", invokeInput);
+
+        /*
+        * Event handler for dblclick Listner
+        * 
+        */
+
+        function invokeInput(e) {
             var input; 
+            var backspace = 8;
+            var storageValue = localStorage[e.target.id] || "";
 
             if ( e.target.id.startsWith("row-head") 
                 || e.target.id.startsWith("col-head")) {
                 return;
             }
 
+            // backspace produce back page changing-------------------------------------------------------------------------------
+
+            // if (e.keyCode === backspace) {
+            //     e.target.innerHTML = "";
+            //     localStorage[e.target.id] = "";
+            //     return;
+            // }
+
             input = document.createElement("input");
-            input.value = e.target.innerHTML;
+            input.value = storageValue;
             e.target.innerHTML = "";
             e.target.appendChild(input);
             input.focus();
-            input.addEventListener("blur", doneClick);
-            input.addEventListener("keyup", doneEnter);
 
-            function doneClick(e) {
-                this.parentNode.innerHTML = this.value;
+            // cant catch event exception------------------------------------------------------------------------------------------
+
+            input.addEventListener("blur", doneClick);
+            input.addEventListener("keydown", doneEnter);
+
+            function doneClick() {
+                this.parentNode.innerHTML = compute(this.value);
+                if (this.value) {
+                    localStorage[e.target.id] = this.value;
+                } 
             }
 
             function doneEnter(e) {
-                if (e.keyCode === 13) this.blur();
+                var enterKey = 13;
+                if (e.keyCode === enterKey) {
+                    this.blur();
+                }
             }
         }
     }
 
-/*
-*
-*
-*
-*
-*
-*
-*/
+    //need verification of value in storage(ket = data-cell....)
 
+    function loadData() {
+        if (localStorage.length) {
+            for(elem in localStorage) {
+                var value = localStorage[elem];
+                value = compute(value);
+                document.getElementById(elem).innerHTML = value || "";
+            }
+        } else {
+
+        }
+    }
+
+    /*
+    *
+    *Computing expressions from storage or database
+    *
+    *@param {string} expression
+    *
+    *@return {primitive} computed result
+    */
+
+    // Formula handler will add here
+    function compute(value) {
+        if (value.charAt(0) == "=") {
+                value = eval(value.substring(1));
+            }
+        return value;
+    }
 
 })();
